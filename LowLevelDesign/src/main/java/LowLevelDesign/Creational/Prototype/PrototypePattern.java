19464baa5dbce52f4f211e38807dea00ff87ae8c
Anurag Singh Patel <anuragsinghpatel98@gmail.com>
2024-11-21 20:37:36 +0530

    updating main method for test prototype

--- LowLevelDesign/src/main/java/LowLevelDesign/Creational/Prototype/PrototypePattern.java
+++ LowLevelDesign/src/main/java/LowLevelDesign/Creational/Prototype/PrototypePattern.java
@@ -1,4 +1,25 @@
 package LowLevelDesign.Creational.Prototype;
 
 public class PrototypePattern {
+    public static void main(String[] args) {
+        // Create an initial document (prototype)
+        ReportDocument originalReport = new ReportDocument(
+                "Monthly Report",
+                "This is the content of the monthly report.",
+                "Confidential - For Internal Use Only"
+        );
+
+        System.out.println("Original Document: " + originalReport);
+
+        // Clone the original document
+        ReportDocument clonedReport = (ReportDocument) originalReport.clone();
+
+        // Modify the cloned document
+        clonedReport.setTitle("Cloned Monthly Report");
+        clonedReport.setFooter("Public Use Allowed");
+
+        // Display both documents
+        System.out.println("Modified Cloned Document: " + clonedReport);
+        System.out.println("Original Document remains unchanged: " + originalReport);
+    }
 }
