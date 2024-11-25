3c79785f5fbe998291de81d8a53ab9aaaf2e6aa2
Anurag Singh Patel <anuragsinghpatel98@gmail.com>
2024-11-25 20:10:02 +0530

    fix

--- LowLevelDesign/src/main/java/LowLevelDesign/Creational/Singleton/Main.java
+++ LowLevelDesign/src/main/java/LowLevelDesign/Creational/Singleton/Main.java
@@ -6,7 +6,7 @@ public class Main {
         PaymentGateway paymentGateway = PaymentGateway.getInstance();
 
         // Create a payment request
-        PaymentRequest request = new PaymentRequest("Alice", "Bob", 500.75);
+        PaymentRequest request = new PaymentRequest("User1", "User2", 500.75);
 
         // Process the payment
         paymentGateway.processPayment(request);
