7ee0abd3f274bc3b3d42e01726899a553544f7bf
Anurag Singh Patel <anuragsinghpatel98@gmail.com>
2024-11-29 19:29:57 +0530

    updting main method

--- LowLevelDesign/src/main/java/LowLevelDesign/Structural/Adapter/AdapterPatternDemo.java
+++ LowLevelDesign/src/main/java/LowLevelDesign/Structural/Adapter/AdapterPatternDemo.java
@@ -4,9 +4,9 @@ public class AdapterPatternDemo {
     public static void main(String[] args) {
         MediaPlayer audioPlayer = new AudioPlayer();
 
-        audioPlayer.play("mp3", "song.mp3");
-        audioPlayer.play("mp4", "video.mp4");
-        audioPlayer.play("vlc", "movie.vlc");
-        audioPlayer.play("avi", "unsupported.avi");
+        audioPlayer.play("MP3", "song.mp3");
+        audioPlayer.play("MP4", "video.mp4");
+        audioPlayer.play("VLC", "movie.vlc");
+        audioPlayer.play("AVI", "unsupported.avi");
     }
 }
