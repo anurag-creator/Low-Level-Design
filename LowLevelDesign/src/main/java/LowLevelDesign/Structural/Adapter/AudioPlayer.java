85dd3474de526b4cb571f1833f8cccc0f6f5a39e
Anurag Singh Patel <anuragsinghpatel98@gmail.com>
2024-11-29 19:30:40 +0530

    fix

--- LowLevelDesign/src/main/java/LowLevelDesign/Structural/Adapter/AudioPlayer.java
+++ LowLevelDesign/src/main/java/LowLevelDesign/Structural/Adapter/AudioPlayer.java
@@ -6,7 +6,7 @@ public class AudioPlayer implements MediaPlayer {
     @Override
     public void play(String audioType, String fileName) {
         if (audioType.equalsIgnoreCase("mp3")) {
-            System.out.println("Playing mp3 file: " + fileName);
+            System.out.println("Playing MP3 file: " + fileName);
         } else if (audioType.equalsIgnoreCase("vlc") || audioType.equalsIgnoreCase("mp4")) {
             mediaAdapter = new MediaAdapter(audioType);
             mediaAdapter.play(audioType, fileName);
