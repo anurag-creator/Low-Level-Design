7d2338f6f514497dfeceef3d49ba59db2b7e671d
Anurag Singh Patel <anuragsinghpatel98@gmail.com>
2024-12-03 19:40:09 +0530

    removing

--- LowLevelDesign/src/main/java/LowLevelDesign/Structural/Bridge/CircleShape.java
+++ LowLevelDesign/src/main/java/LowLevelDesign/Structural/Bridge/CircleShape.java
@@ -3,9 +3,6 @@ package LowLevelDesign.Structural.Bridge;
 public class CircleShape implements Shape {
     private Color color;
 
-    public CircleShape(Color color) {
-        this.color = color;
-    }
 
     @Override
     public void draw() {
