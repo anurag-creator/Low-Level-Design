ac362f1648305ead85674992a0a637db2db6b941
Anurag Singh Patel <anuragsinghpatel98@gmail.com>
2024-12-03 19:39:18 +0530

    adding logs

--- LowLevelDesign/src/main/java/LowLevelDesign/Structural/Bridge/Circle.java
+++ LowLevelDesign/src/main/java/LowLevelDesign/Structural/Bridge/Circle.java
@@ -2,6 +2,6 @@ package LowLevelDesign.Structural.Bridge;
 
 public class Circle implements Shape {
     public void draw(){
-        
+        System.out.println("Circle shape drawing... ");
     }
 }
